fe98754f1ec69a4596096a0ab80ae3ecedbc83ce
Enigma <49351666+AwokeinanEnigma@users.noreply.github.com>
2023-08-05 19:00:13 -0400

    remove debug statements

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -13,21 +13,13 @@ namespace RotatingHelloWorldSfmlDotNetCoreCSharp
         {
             Engine.Initialize();
             SceneManager.Instance.Push(new GUIText());
-            DDDebug.Log("1");
-            //SceneManager.Instance.Push(new TileScene());
             Engine.DebugMode = true;
             Engine.Window.SetMouseCursorVisible(false);
-
             
-            DDDebug.Log("3");
-      
             new AxisManager();
-            DDDebug.Log("5");
  
             Console.WriteLine("Press ESC key to close window");
-            DDDebug.Log("7");
             Engine.StartGameLoop();
-            DDDebug.Log("9");
         }
     }
 }
